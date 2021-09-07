70f16f7582a4d74001d0700a7628e8e6cf5769bb
jooeui <kje_0727@naver.com>
2021-09-07 18:00:36 +0900

    21-09-07 ch03 클래스

---
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -0,0 +1,15 @@
+package chapter03;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		Goods goods = new Goods();
+		goods.setName("Nikon");
+		goods.setPrice(400000);
+		goods.setCountStock(30);
+		goods.setCountSold(50);
+		
+		System.out.println(goods.toString());
+	}
+
+}
