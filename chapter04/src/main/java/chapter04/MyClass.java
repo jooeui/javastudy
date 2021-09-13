02a96484f92580b0376f3e51550a2adf293e563c
jooeui <kje_0727@naver.com>
2021-09-13 20:00:46 +0900

    21-09-13 수업내용

--- chapter04/src/main/java/chapter04/MyClass.java
+++ chapter04/src/main/java/chapter04/MyClass.java
@@ -8,6 +8,8 @@ public class MyClass {
 	
 	// Singleton + Factory Method
 	public static MyClass getInstance() {
+		// return new MyClass();	// Factory Method는 되지만 Singleton x
+		
 		if(instance == null) {
 			instance = new MyClass();
 		}
