8068d719467b8b75a74e4857b526ec4f23e1f664
jooeui <kje_0727@naver.com>
2021-09-16 11:15:11 +0900

    21-09-16 httpd #1

--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -1,10 +1,6 @@
 package echo;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -22,39 +18,9 @@ public class EchoServer {
 			serverSocket.bind(new InetSocketAddress("0.0.0.0", PORT));		// Socket에 InetSocketAddress(IPAddress + Port) binding
 			log("starts... [port:" + PORT + "]");
 			
-			Socket socket = serverSocket.accept();	// blocking
-			
-			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
-			String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
-			int remoteHostPort = inetRemoteSocketAddress.getPort();
-			log("connected by client[" + remoteHostAddress + ":" + remoteHostPort + "]");
-			
-			try {
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-				
-				// flush true - 쓸 때마다 자동으로 flush(autoFlush), false로 하면 명시적으로 flush를 써줘야 함
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
-				
-				while(true) {
-					String data = br.readLine();
-					if(data == null) {
-						log("closed by client");
-						break;
-					}
-					
-					log("received: " + data);
-					pw.println(data);
-				}
-			} catch(IOException e) {
-				log("error: " + e);
-			} finally {
-				try {
-					if(socket != null && socket.isClosed() == false) {
-						socket.close();
-					}
-				} catch(IOException e) {
-					e.printStackTrace();
-				}
+			while(true) {
+				Socket socket = serverSocket.accept();	// blocking
+				new EchoServerReceiveThread(socket).start();
 			}
 		} catch (IOException e) {
 			log("error: " + e);
